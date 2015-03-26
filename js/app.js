d149bc4b4613b3848c233550e15902d0b93f905a
Antonio Stoilkov <antonio.stoilkov@gmail.com>
2015-03-26 10:03:47 +0200

    initial implementation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,120 @@
-(function (window) {
+(function () {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	var ENTER_KEY = 13;
+	var ESCAPE_KEY = 27;
 
-})(window);
+	var App = blocks.Application();
+
+	var Todo = App.Model({
+		title: App.Property(),
+
+		completed: App.Property(),
+
+		editing: blocks.observable(),
+
+		init: function () {
+			var collection = this.collection();
+			if (collection) {
+				this.title.on('change', collection.save);
+				this.completed.on('change', collection.save);
+			}
+		},
+
+		toggleComplete: function () {
+			this.completed(!this.completed());
+		},
+
+		edit: function (e) {
+			this.lastValue = this.title();
+			this.editing(true);
+		},
+
+		closeEdit: function () {
+			this.editing(false);
+		},
+
+		handleAction: function (e) {
+			if (e.which == ENTER_KEY) {
+				this.editing(false);
+			} else if (e.which == ESCAPE_KEY) {
+				this.title(this.lastValue);
+				this.editing(false);
+			}
+		}
+	});
+
+	var Todos = App.Collection(Todo, {
+		remaining: blocks.observable(),
+
+		init: function () {
+			this
+				.reset(JSON.parse(localStorage.getItem('todos-jsblocks')))
+				.on('add remove', this.save)
+				.updateRemaining();
+		},
+
+		toggleAll: function () {
+			var complete = this.remaining() == 0 ? false : true;
+			this.each(function (todo) {
+				todo.completed(complete);
+			});
+		},
+
+		clearCompleted: function () {
+			this.removeAll(function (todo) {
+				return todo.completed();
+			});
+		},
+
+		save: function () {
+			var result = [];
+
+			blocks.each(this(), function (model) {
+				result.push(model.dataItem());
+			});
+
+			localStorage.setItem('todos-jsblocks', JSON.stringify(result));
+
+			this.updateRemaining();
+		},
+
+		updateRemaining: function () {
+			this.remaining(this.reduce(function (memo, todo) {
+				return todo.completed() ? memo : memo + 1;
+			}, 0));
+		}
+	});
+
+	App.View('Todos', {
+		options: {
+			route: blocks.route('{{filter}}').optional('filter')
+		},
+
+		filter: blocks.observable(),
+
+		newTodo: Todo(),
+
+		todos: Todos().extend('filter', function (value) {
+			var mode = this.filter();
+			return mode == 'active' ? !value.completed() :
+				mode == 'completed' ? value.completed() :
+				true;
+		}),
+
+		routed: function (params) {
+			if (params.filter != 'active' && params.filter != 'completed') {
+				params.filter = 'all';
+			}
+			this.filter(params.filter);
+		},
+
+		addTodo: function (e) {
+			if (e.which == ENTER_KEY) {
+				this.todos.push(this.newTodo);
+				this.newTodo.reset();
+			}
+		}
+	});
+
+})();
--- readme.md
+++ readme.md
@@ -1,19 +1,23 @@
-# TodoMVC App Template
+# jsblocks • [TodoMVC](http://todomvc.com)
 
-> Template used for creating [TodoMVC](http://todomvc.com) apps
+From simple user interfaces to complex single-page applications using faster, server-side rendered and easy to learn framework
 
-![](https://github.com/tastejs/todomvc-app-css/raw/master/screenshot.png)
+## Resources
 
+- [Website](http://104.131.138.38)
+- [Documentation](http://104.131.138.38/learn)
+- [Download](http://104.131.138.38/download)
+- [GitHub Page](https://github.com/astoilkov/jsblocks)
 
-## Getting started
+### Support
 
-- Read the [Application Specification](https://github.com/tastejs/todomvc/blob/master/app-spec.md) before touching the template.
+- [StackOverflow](http://stackoverflow.com/questions/tagged/jsblocks)
+- [Google Groups](https://groups.google.com/forum/#!forum/jsblocks)
+- [Twitter](http://twitter.com/jsblocks)
+- [Google+](https://plus.google.com/communities/100030562502977783693)
 
-- Delete this file and rename `app-readme.md` to `readme.md` and fill it out.
+*Let us [know](https://github.com/tastejs/todomvc/issues) if you discover anything worth sharing.*
 
-- Clone this repo and install the dependencies with [npm](https://npmjs.com) by running: `npm install`.
+## Credit
 
-
-## License
-
-<a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/4.0/80x15.png" /></a><br />This <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dct:type">work</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://sindresorhus.com" property="cc:attributionName" rel="cc:attributionURL">TasteJS</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.en_US">Creative Commons Attribution 4.0 International License</a>.
+Created by [Antonio Stoilkov](http://your-website.com)
